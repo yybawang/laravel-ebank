11468b915868424b29fb9963afee4e70e984d36d
yybawang <512511253@qq.com>
2018-09-10 11:30:40 +0800

    :art: 日志记录改为每日记录

--- config/app.php
+++ config/app.php
@@ -124,6 +124,8 @@ return [
     'log' => env('APP_LOG', 'single'),
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
+	
+	'log_max_files' => 30,
 
     /*
     |--------------------------------------------------------------------------
