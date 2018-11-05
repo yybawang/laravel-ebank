a6b3ab56169fc35085519a6f45e36cf76b4b24a0
yybawang <512511253@qq.com>
2018-11-05 14:46:39 +0800

    :sparkles: queue:work-listen 支持所有参数

--- app/Console/Commands/QueueWorkListen.php
+++ app/Console/Commands/QueueWorkListen.php
@@ -14,8 +14,13 @@ class QueueWorkListen extends Command
     protected $signature = 'queue:work-listen
                             {connection? : The name of the queue connection to work}
                             {--queue= : The names of the queues to work}
+                            {--daemon : Run the worker in daemon mode (Deprecated)}
                             {--delay=0 : The number of seconds to delay failed jobs}
+                            {--force : Force the worker to run even in maintenance mode}
+                            {--memory=128 : The memory limit in megabytes}
+                            {--once : Only process the next job on the queue}
                             {--sleep=3 : Number of seconds to sleep when no job is available}
+                            {--timeout=60 : The number of seconds a child process can run}
                             {--tries=0 : Number of times to attempt a job before logging it failed}';
 
     /**
@@ -43,11 +48,16 @@ class QueueWorkListen extends Command
     public function handle()
     {
         $this->call('queue:work',[
-        	'--queue'	=> queue_name($this->option('queue') ?: 'default'),
-			'--delay'	=> $this->option('delay'),
-			'--sleep'	=> $this->option('sleep'),
-			'--tries'	=> $this->option('tries'),
-			'--quiet'	=> true,
+			'--connection'	=> $this->option('connection'),
+        	'--queue'		=> queue_name($this->option('queue') ?: 'default'),
+			'--daemon'		=> $this->option('daemon'),
+			'--delay'		=> $this->option('delay'),
+			'--force'		=> $this->option('force'),
+			'--memory'		=> $this->option('memory'),
+			'--once'		=> $this->option('once'),
+			'--sleep'		=> $this->option('sleep'),
+			'--timeout'		=> $this->option('timeout'),
+			'--tries'		=> $this->option('tries'),
 		]);
     }
 }
