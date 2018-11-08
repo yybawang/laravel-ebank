de3ff54196d91b4cca6e3cf207588e565b513d6c
yanyong <512511253@qq.com>
2018-11-08 22:55:35 +0800

    :lipstick: quque-listen 支持所有参数

--- app/Console/Commands/QueueWorkListen.php
+++ app/Console/Commands/QueueWorkListen.php
@@ -14,8 +14,13 @@ class QueueWorkListen extends Command
     protected $signature = 'queue:work-listen
                             {connection? : The name of the queue connection to work}
                             {--queue= : The names of the queues to work}
+                            {--daemon : Run the worker in daemon mode (Deprecated)}
+                            {--once : Only process the next job on the queue}
                             {--delay=0 : The number of seconds to delay failed jobs}
+                            {--force : Force the worker to run even in maintenance mode}
+                            {--memory=128 : The memory limit in megabytes}
                             {--sleep=3 : Number of seconds to sleep when no job is available}
+                            {--timeout=60 : The number of seconds a child process can run}
                             {--tries=0 : Number of times to attempt a job before logging it failed}';
 
     /**
@@ -43,11 +48,16 @@ class QueueWorkListen extends Command
     public function handle()
     {
         $this->call('queue:work',[
+        	'connection'=> $this->argument('connection'),
         	'--queue'	=> queue_name($this->option('queue') ?: 'default'),
+			'--daemon'	=> $this->option('daemon'),
+			'--once'	=> $this->option('once'),
 			'--delay'	=> $this->option('delay'),
+			'--force'	=> $this->option('force'),
+			'--memory'	=> $this->option('memory'),
 			'--sleep'	=> $this->option('sleep'),
+			'--timeout'	=> $this->option('timeout'),
 			'--tries'	=> $this->option('tries'),
-			'--quiet'	=> true,
 		]);
     }
 }
