cb62226cad9d2c764ac2d50f40589f5b7432e762
eric <512511253@qq.com>
2020-07-21 15:07:48 +0800

    删除js logger 方法

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -14,18 +14,6 @@ try {
     // require('bootstrap');
 } catch (e) {}
 
-window.logger = function(...args){
-    if(process.env.NODE_ENV !== 'production'){
-        for(let i in args){
-            console.log(i+'-----------------------------------  start');
-            console.info(typeof args[i]);
-            console.log(args[i]);
-            console.log(i+'-----------------------------------  end');
-        }
-    }
-};
-
-
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
